684130dacdc6992fbbd2902d551c623e3c5235d2
Asif Saif Uddin <auvipy@gmail.com>
2017-12-11 13:26:51 +0600

    clean up in cloudlet api code

--- server/register-rest/cloudlet/api.py
+++ server/register-rest/cloudlet/api.py
@@ -1,7 +1,6 @@
-from django.utils.timezone import utc
 from django.utils.timezone import now
 import heapq
-import socket 
+import socket
 from operator import itemgetter
 from tastypie.authorization import Authorization
 from .models import Cloudlet
@@ -20,11 +19,16 @@ cost = ip_location.IPLocation()
 
 class PrettyJSONSerializer(Serializer):
     json_indent = 2
+
     def to_json(self, data, options=None):
         options = options or {}
         data = self.to_simple(data, options)
-        return simplejson.dumps(data, cls=json.DjangoJSONEncoder,
-                sort_keys=True, ensure_ascii=False, indent=self.json_indent)
+        return simplejson.dumps(
+            data,
+            cls=json.DjangoJSONEncoder,
+            sort_keys=True, ensure_ascii=False,
+            indent=self.json_indent
+        )
 
 
 class CloudletResource(ModelResource):
@@ -39,8 +43,8 @@ class CloudletResource(ModelResource):
         resource_name = 'Cloudlet'
         list_allowed_methods = ['get', 'post', 'put', 'delete']
         excludes = ['pub_date', 'mod_time', 'id']
-        filtering = {"mod_time":ALL, "status":ALL, "ip_address":ALL,
-                "latitude":ALL, "longitude":ALL, "rest_api_port":ALL}
+        filtering = {"mod_time": ALL, "status": ALL, "ip_address": ALL,
+                     "latitude": ALL, "longitude": ALL, "rest_api_port": ALL}
 
     def obj_create(self, bundle, **kwargs):
         '''
@@ -73,14 +77,14 @@ class CloudletResource(ModelResource):
         '''
         called for POST, UPDATE, GET
         '''
-        #bundle.data['longitude'] = "%9.6f" % bundle.data['longitude']
-        #bundle.data['latitude'] = "%9.6f" % bundle.data['latitude']
+        # bundle.data['longitude'] = "%9.6f" % bundle.data['longitude']
+        # bundle.data['latitude'] = "%9.6f" % bundle.data['latitude']
         return bundle
 
     def prepend_urls(self):
         return [url(r"^(?P<resource_name>%s)/search%s$" %
-                (self._meta.resource_name, trailing_slash()),
-                self.wrap_view('get_search'), name="api_get_search"), ]
+                    (self._meta.resource_name, trailing_slash()),
+                    self.wrap_view('get_search'), name="api_get_search"), ]
 
     def get_search(self, request, **kwargs):
         self.method_check(request, allowed=['get'])
@@ -97,10 +101,10 @@ class CloudletResource(ModelResource):
             if client_ip is None or self._is_ip(client_ip) is False:
                 client_ip = request.META.get("REMOTE_ADDR")
             client_location = cost.ip2location(str(client_ip))
-            latitude = getattr(client_location, 'latitude', \
-                    CloudletResource.DEFAULT_LATITUDE)
-            longitude = getattr(client_location, 'longitude', \
-                    CloudletResource.DEFAULT_LONGITUDE)
+            latitude = getattr(client_location, 'latitude',
+                               CloudletResource.DEFAULT_LATITUDE)
+            longitude = getattr(client_location, 'longitude',
+                                CloudletResource.DEFAULT_LONGITUDE)
         cloudlet_list = list()
         for cloudlet in Cloudlet.objects.all():
             if cloudlet.status != Cloudlet.CLOUDLET_STATUS_RUNNING:
@@ -109,15 +113,19 @@ class CloudletResource(ModelResource):
                 cloudlet.latitude = str(CloudletResource.DEFAULT_LATITUDE)
             if len(cloudlet.longitude) is 0:
                 cloudlet.longitude = str(CloudletResource.DEFAULT_LONGITUDE)
-            geo_distance = ip_location.geo_distance(latitude, longitude, \
-                    float(cloudlet.latitude), float(cloudlet.longitude))
+            geo_distance = ip_location.geo_distance(
+                latitude, longitude,
+                float(cloudlet.latitude),
+                float(cloudlet.longitude)
+            )
             cloudlet.cost = geo_distance
             cloudlet_list.append(cloudlet)
 
-        top_cloudlets = heapq.nsmallest(SEARCH_COUNT, cloudlet_list, key=itemgetter('cost'))
+        top_cloudlets = heapq.nsmallest(
+            SEARCH_COUNT, cloudlet_list, key=itemgetter('cost'))
         top_cloudlet_list = [item.search_out() for item in top_cloudlets]
         object_list = {
-            'cloudlet' : top_cloudlet_list
+            'cloudlet': top_cloudlet_list
         }
         self.log_throttled_access(request)
         return self.create_response(request, object_list)
@@ -139,5 +147,5 @@ def post_save_signal(sender, **kwargs):
     redis.set(cloudlet.ip_address, (cloudlet.latitude, cloudlet.longitude))
     '''
 
-post_save.connect(post_save_signal, sender=Cloudlet)
 
+post_save.connect(post_save_signal, sender=Cloudlet)
