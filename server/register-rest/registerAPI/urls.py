02ee872e03325c3c08a8ae3ff0924d1472da9ad4
Asif Saif Uddin <auvipy@gmail.com>
2017-12-11 15:24:09 +0600

    url style cleanup

--- server/register-rest/registerAPI/urls.py
+++ server/register-rest/registerAPI/urls.py
@@ -11,15 +11,15 @@ v1_api.register(CloudletResource())
 
 
 urlpatterns = patterns('',
-    # Examples:
-    # url(r'^$', 'registerAPI.views.home', name='home'),
-    # url(r'^registerAPI/', include('registerAPI.foo.urls')),
+                       # Examples:
+                       # url(r'^$', 'registerAPI.views.home', name='home'),
+                       # url(r'^registerAPI/', include('registerAPI.foo.urls')),
 
-    # Uncomment the admin/doc line below to enable admin documentation:
-    # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
+                       # Uncomment the admin/doc line below to enable admin documentation:
+                       # url(r'^admin/doc/', include('django.contrib.admindocs.urls')),
 
-    # Uncomment the next line to enable the admin:
-    # url(r'^admin/', include(admin.site.urls)),
+                       # Uncomment the next line to enable the admin:
+                       # url(r'^admin/', include(admin.site.urls)),
 
-    (r'^api/', include(v1_api.urls)),
-)
+                       (r'^api/', include(v1_api.urls)),
+                       )
