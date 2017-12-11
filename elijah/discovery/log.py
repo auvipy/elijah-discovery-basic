5859e331ed663121a98d04ddd0d71be0d4dce16f
Asif Saif Uddin <auvipy@gmail.com>
2017-12-11 15:49:47 +0600

    elijah log clean up

--- elijah/discovery/log.py
+++ elijah/discovery/log.py
@@ -1,4 +1,4 @@
-#!/usr/bin/env python 
+#!/usr/bin/env python
 #
 # Cloudlet Infrastructure for Mobile Computing
 #
@@ -18,51 +18,22 @@
 #   limitations under the License.
 #
 
-from config import DiscoveryConst as Const
-import os
 import logging
-import sys
+
 
 loggers = dict()
 DEFAULT_FORMATTER = '%(asctime)s %(name)s %(levelname)s %(message)s'
 
+
 def getLogger(name='unknown'):
-    if loggers.get(name, None) == None:
-        # default file logging
-        ''' log_filepath = "/var/tmp/cloudlet/log-discovery"
-        if hasattr(Const, "LOG_PATH") == True:
-            log_filepath = Const.LOG_PATH
-        if os.path.exists(os.path.dirname(log_filepath)) == False:
-            os.makedirs(os.path.dirname(log_filepath))
-            os.chmod(os.path.dirname(log_filepath), 0o777)
-        if os.path.exists(log_filepath) == False:
-            open(log_filepath, "w+").close()
-        os.chmod(log_filepath, 0o777)
-        logging.basicConfig(level=logging.DEBUG,
-                format='%(asctime)s %(name)-12s %(levelname)-8s %(message)s',
-                datefmt='%m-%d %H:%M',
-                filename=log_filepath,
-                filemode='a')
-        hdlr = logging.FileHandler(log_filepath)
-        hdlr.setLevel(logging.DEBUG)
-        formatter = logging.Formatter(DEFAULT_FORMATTER)
-        hdlr.setFormatter(formatter)
-        '''
+    if loggers.get(name, None) is None:
         logger = logging.getLogger(name)
-        logging.basicConfig(level=logging.DEBUG,
-                format='%(asctime)s %(levelname)-8s %(message)s',
-                datefmt='%m-%d %H:%M')
+        logging.basicConfig(
+            level=logging.DEBUG,
+            format='%(asctime)s %(levelname)-8s %(message)s',
+            datefmt='%m-%d %H:%M'
+        )
 
-        # add stdout logging with INFO level
-        '''
-        console = logging.StreamHandler(sys.stdout)
-        console.setLevel(logging.DEBUG)
-        formatter = logging.Formatter('%(levelname)-8s %(message)s')
-        console.setFormatter(formatter)
-        logger.addHandler(console)
-        '''
-
-        loggers[name] = logger
+        loggers["name"] = logger
 
     return loggers.get(name)
-
